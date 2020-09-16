8ed55cda267555ff157346fe84e36a16b696b43c
Sam Kenpachi <samuelkabelo1@gmail.com>
2020-09-16 23:32:55 +0200

    integrating tabs to the profile page

--- profile.js
+++ profile.js
@@ -1,10 +1,28 @@
+// tab components
+Vue.component("tab-email", {
+    template: "<h4>email tab</h4>"
+});
+Vue.component("tab-twitter", {
+    template: "<h4>twitter tab</h4>"
+});
+Vue.component("tab-github", {
+    template: "<h4>github tab</h4>"
+});
+
 var app = new Vue({
     el: "#profile",
     data: {
         name: "Sam Kenpachi",
         work: "Freelance Developer",
         location: "Location: Botswana-Gaborone",
-        img: './img.png'
+        img: './img.png',
+        currentTab: "Email",
+        tabs: ["Email", "Twitter", "GitHub"]
+    },
+    computed: {
+        currentTabComponent: function() {
+            return "tab-" + this.currentTab.toLowerCase();
+        }
     }
 
 });
